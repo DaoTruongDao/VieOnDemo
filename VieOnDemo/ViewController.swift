8df54aa119e39b171921e8fc0ba98a7542bd71b8
Đạo <daoprovip2014@gmail.com>
2021-07-24 13:22:34 +0700

    createViewLogin

--- VieOnDemo/ViewController.swift
+++ VieOnDemo/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var txtPhone: UITextField!
+    @IBOutlet weak var txtPass: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let phoneImage = UIImage(named: "phone")
+        addLeftImage(txtField: txtPhone, andImage: phoneImage!)
+        
+        let passImage = UIImage(named: "key")
+        addLeftImage(txtField: txtPass, andImage: passImage!)
     }
-
-
+    func addLeftImage(txtField: UITextField, andImage img: UIImage){
+        let leftImageView = UIImageView(frame: CGRect(x: 0.0, y: 0.0, width: img.size.width, height: img.size.height))
+        leftImageView.image = img
+        txtField.leftView = leftImageView
+        txtField.leftViewMode = .always
+    }
 }
+    
+
 
