e083d37227103a66a756a91a54c5aa7dd9fa7173
Đạo <daoprovip2014@gmail.com>
2021-07-24 13:27:06 +0700

    fixAppDelegate

--- VieOnDemo/AppDelegate.swift
+++ VieOnDemo/AppDelegate.swift
@@ -13,9 +13,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+          // Override point for customization after application launch.
+          window = UIWindow(frame: UIScreen.main.bounds)
+          window?.backgroundColor = .white
+          let vc = ViewController()
+          window?.rootViewController = vc
+          window?.makeKeyAndVisible()
+          
+          return true
     }
+    
 
     // MARK: UISceneSession Lifecycle
 
