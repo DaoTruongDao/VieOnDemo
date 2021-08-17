9939837a9493a74bb6dc8b4fa1d1167a62d5d489
Đạo <daoprovip2014@gmail.com>
2021-08-17 11:52:54 +0700

    khoá xoay màn hình

--- VieOnDemo/ViewController.swift
+++ VieOnDemo/ViewController.swift
@@ -22,6 +22,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var background: UIImageView!
     
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -62,5 +63,23 @@ class ViewController: UIViewController {
             txtPass.isSecureTextEntry = true
         }
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        AppUtility.lockOrientation(.portrait)
+        // Or to rotate and lock
+        // AppUtility.lockOrientation(.portrait, andRotateTo: .portrait)
+        
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Don't forget to reset when view is being removed
+        AppUtility.lockOrientation(.all)
+    }
+    
+   
     
 }
